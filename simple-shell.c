10bf8e5e6d72267e86b8f134a55a2d4f1c7f81e6
quangdzuytran <quangduy0412@gmail.com>
2020-11-01 19:41:15 +0700

    Minor fixes and code refactoring

--- simple-shell.c
+++ simple-shell.c
@@ -3,39 +3,49 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <stdbool.h> //type bool
+// #include <stdbool.h> //type bool
 #define MAX_LENGTH	80 // The maximum length command
 #define MAX_ARGS	MAX_LENGTH/2 //The maximum number of arguments
 char temp[MAX_LENGTH];
 
 //split string command to arguments
 char** parse_arguments(char *command) {
-	char **args = (char**)malloc(MAX_ARGS * sizeof(char *));
+	char delim[] = " \n\t\r\a";
+	unsigned int args_size = MAX_ARGS;
+	char** args = (char**)malloc(args_size * sizeof(char*));
 	strcpy(temp, command);
-    char *param;
-    param = strtok(temp, " ");
+    char* param;
+    param = strtok(temp, delim);
     int size = 0;
     while (param != NULL) {
-       	args[size++] = param;
-        param = strtok(NULL, " ");
+       	args[size] = param;
+		if (++size >= args_size) {
+			args_size += MAX_ARGS;
+			args = (char**)realloc(args, args_size * sizeof(char*));
+			if (!args) {
+				perror("Allocaion error");
+				exit(EXIT_FAILURE);
+			}
+		}
+        param = strtok(NULL, delim);
     }
     args[size] = NULL;
     return args;
 }
 
-void execCommand(char* command) {
+void exec_command(char* command) {
 	if (strcmp(command, "") == 0) {
 		return;
 	}
 	char** args = parse_arguments(command);
 	//Fork a child process 
 	pid_t child_pid = fork();
-	bool waitChild = 1;
+	int waitChild = 1;
 	if (child_pid < 0) {
 		//Parent process, but child couldn't be created
 		perror("Fork failed");
 		free(args); // free up memory for arguments
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	if (child_pid == 0) { 
@@ -43,7 +53,7 @@ void execCommand(char* command) {
 		if (execvp(args[0], args) < 0) { // catch error
 			perror(args[0]);
 			free(args); // free up memory for arguments
-			exit(1);
+			exit(EXIT_FAILURE);
 		}
 	} 
 	else if (child_pid > 0) { 
@@ -54,23 +64,21 @@ void execCommand(char* command) {
 	free(args);
 }
 
-int main(void) {
+int main() {
 	char command[MAX_LENGTH];
 	int should_run = 1; /* flag to determine when to exit program */
 	while (should_run) {
 		printf("ssh>");
 		fflush(stdout);
 		fgets(command, MAX_LENGTH, stdin);
-		char *pos = strchr(command, '\n');
-		if (pos)
-			*pos = 0;
 		char** args = parse_arguments(command);
-		if (strcmp("exit", args[0])==0){
+		if (strcmp("exit", args[0]) == 0){
+			free(args);
 			should_run = 0;
 			break;
 		}
 		free(args);
-		execCommand(command);
+		exec_command(command);
 	}
 	return 0;
 } 
