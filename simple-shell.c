afbde750f40289023505c3ba8682e31b28dd0558
somethingintheway <nguyenduongag@gmail.com>
2020-11-07 12:24:57 +0700

    Done pipe fuction

--- simple-shell.c
+++ simple-shell.c
@@ -14,6 +14,32 @@
 #define OUTPUT_REDIRECTION 2
 // char temp[MAX_LENGTH];
 
+int isPipeCommand(char **args){
+	//Check if the command only has one pipe character and no other redirection operators
+	int i = 0;
+	int nPipeChar = 0;
+	int pos = -1;
+	while(args[i] != NULL)
+	{
+		if (strcmp("<", args[i])==0 || strcmp(">", args[i])==0 ){
+			return false;
+		}
+		else if (strcmp("|", args[i])==0){
+			nPipeChar++;
+			pos = i;
+		}
+		i++;
+	}
+	if (nPipeChar==1)
+	{
+		//asign the pipe argument as NULL to divide between 2 commands
+		args[pos] = NULL;
+		return pos;
+	}
+	return -1;
+}
+
+
 //split string command to arguments
 char** parse_arguments(char *command) {
 	char delim[] = " \n\t\r\a";
@@ -56,6 +82,85 @@ int cd(char *path) {
     return chdir(path);
 }
 
+bool execPipeCommand(char **cmd1, char **cmd2, bool hasAmpersand)
+{
+	int pd[2]; //0 end is reading, 1 end is writing
+	if (pipe(pd) < 0)
+	{
+		printf("Cannot create pipe!\n");
+		return 0;
+	}
+	pid_t pid2, pid1; 
+	pid1 = fork();
+	if (pid1 < 0)
+	{
+		printf("Cannot fork first command\n");
+		return 0;
+	}
+	if (pid1 == 0)
+	{
+		//child 1 process
+		//we only need to write here so we close the reading end
+		close(pd[0]);
+		//copying the file descriptor of STD_OUT to pd[1]
+		dup2(pd[1], STDOUT_FILENO);
+		//descriptor alreadly duplicated, the old pd[1] is no longer needed
+		close(pd[1]);
+
+		if (execvp(cmd1[0], cmd1) < 0)
+		{
+			perror(cmd1[0]);
+			return 0;
+		}
+	}
+	else
+	{
+		//parent process
+		pid2 = fork();
+		if (pid2 < 0)
+		{
+			printf("Cannot fork second command\n");
+			return 0;
+		}
+
+		if (pid2 == 0)
+		{
+			//child 2 process
+			//we only need to read here so we close the writing end
+			close(pd[1]);
+			//copying the file descriptor of STD_IN to pd[0]
+			dup2(pd[0], STDIN_FILENO);
+			close(pd[0]);
+			if (execvp(cmd2[0], cmd2) < 0)
+			{
+				perror(cmd2[0]);
+				return 0;
+			}
+		}
+		else
+		{
+			//Process wait if command have &
+			if (!hasAmpersand)
+			{
+				waitpid(pid1, NULL, 0); 
+            	waitpid(pid1, NULL, 0); 
+
+			}
+			//Since file descriptors are inherited(copied) when calling fork(), they still exits in the parent process
+			//Therefore, we have to close the pipe
+			close(pd[0]);
+            close(pd[1]);
+		}
+
+	}
+	//Since file descriptors are inherited(copied) when calling fork()
+	//We have to close the pipe
+	close(pd[0]);
+    close(pd[1]);
+	return true;
+
+}
+
 void exec_command(char* command) {
 	if (strcmp(command, "") == 0) {
 		return;
@@ -64,30 +169,43 @@ void exec_command(char* command) {
 	//check if the last argurment is "&" or not (by using the "hasAmpersand" variable)
 	//if the last argurment is "&" then the parent and child processes will run concurrently
 	bool hasAmpersand = hasAmpersandAtLast(args);
-	//Fork a child process 
-	pid_t child_pid = fork();
-	if (child_pid < 0) {
-		//Parent process, but child couldn't be created
-		perror("Fork failed");
-		free(args); // free up memory for arguments
-		exit(EXIT_FAILURE);
-	}
 
-	if (child_pid == 0) { 
-		//Child process
-		if (execvp(args[0], args) < 0) { // catch error
-			perror(args[0]);
+	int pipePos = isPipeCommand(args);
+	if (pipePos > 0)
+	{
+		if (!execPipeCommand(args, args + pipePos + 1, hasAmpersand))
+		{
 			free(args); // free up memory for arguments
 			exit(EXIT_FAILURE);
 		}
-	} 
-	else if (child_pid > 0) {
-		//Parent process
-		int status = 0;
-		pid_t wpid;
-		//Process wait if command have &
-		if (!hasAmpersand){
-			while((wpid = wait(&status)) > 0);
+	}
+	else
+	{
+		//Fork a child process 
+		pid_t child_pid = fork();
+		if (child_pid < 0) {
+			//Parent process, but child couldn't be created
+			perror("Fork failed");
+			free(args); // free up memory for arguments
+			exit(EXIT_FAILURE);
+		}
+
+		if (child_pid == 0) { 
+			//Child process
+			if (execvp(args[0], args) < 0) { // catch error
+				perror(args[0]);
+				free(args); // free up memory for arguments
+				exit(EXIT_FAILURE);
+			}
+		} 
+		else if (child_pid > 0) {
+			//Parent process
+			int status = 0;
+			pid_t wpid;
+			//Process wait if command have &
+			if (!hasAmpersand){
+				while((wpid = wait(&status)) > 0);
+			}
 		}
 	}
 	free(args);
